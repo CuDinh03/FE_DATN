2df8147495a7db2cd07bd4592ea972db303337b0
CuDinh03 <dinhhuycu0305@gmail.com>
2024-05-09 16:31:50 +0700

    update page, sort by ngay tao

--- src/app/admin/admin-view/admin-view.component.ts
+++ src/app/admin/admin-view/admin-view.component.ts
@@ -1,68 +1,41 @@
-import { Component } from '@angular/core';
-import {AuthenticationService} from "../../service/AuthenticationService";
-import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {TaiKhoanService} from "../../service/TaiKhoanService";
 
 @Component({
   selector: 'app-admin-view',
   templateUrl: './admin-view.component.html',
   styleUrls: ['./admin-view.component.css']
 })
-export class AdminViewComponent {
+export class AdminViewComponent implements OnInit{
 
-  accounts: any[] = []; // Định nghĩa mảng chứa danh sách tài khoản
+  accounts: any[] = [];
+  totalElements = 0;
+  totalPages = 0;
+  currentPage = 0;
+  pageSize = 5;
 
-  role:string = '';
+  startFrom = 1;
 
-  constructor(private http: HttpClient, private auth: AuthenticationService, private router:Router) {
-    this.ngOnInit();
-
-  }
+  constructor(private apiService: TaiKhoanService) {}
 
   ngOnInit(): void {
-    this.getAccounts(); // Gọi phương thức để lấy danh sách tài khoản khi component được khởi tạo
-    console.log(this.auth.getRole());
+    this.loadAccounts();
   }
 
-  getAccounts() {
-    const token = this.auth.getToken();
-    if (!token) {
-      console.error('Token not found or invalid');
-      return;
-    }
-
-    const headers = { 'Authorization': `Bearer ${token}` };
-    this.http.get<any>('http://localhost:9091/api/users/all', { headers }).subscribe(
-      response => {
-        this.accounts = response.result;
-      },
-      error => {
-        if (error.status === 401) {
-          console.error('Unauthorized access, redirecting to login page...');
-          // Redirect or show login modal
-        } else {
-          console.error('Error getting accounts:', error);
-        }
-      }
-    );
-  }
+  loadAccounts(): void {
 
 
-
-  logout() {
-    // Gọi phương thức logout từ AuthenticationService
-    this.auth.logout();
-    // Redirect đến trang đăng nhập sau khi đăng xuất
-    this.router.navigate(['/log-in']).then(() => {
-      console.log('Redirected to /login');
-      this.router.navigate(['/log-in']).then(() => {
-        console.log('Redirected to /log-in');
-      }).catch(err => {
-        console.error('Error navigating to /log-in:', err);
+    this.apiService.getAccounts(this.currentPage, this.pageSize)
+      .subscribe(response => {
+        this.accounts = response.result.content;
+        this.totalElements = response.result.totalElements;
+        this.totalPages = response.result.totalPages;
       });
-    }).catch(err => {
-      console.error('Error navigating to /login:', err);
-    });
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page; // Không cần trừ 1 ở đây
+    this.loadAccounts();
   }
 
 
