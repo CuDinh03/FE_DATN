73f269790280087ac6819bec484f75cde2598905
CuDinh03 <dinhhuycu0305@gmail.com>
2024-06-07 00:06:12 +0700

    update register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
-import { TaiKhoanService } from '../../service/TaiKhoanService';
-import { TaiKhoanDto } from '../../model/tai-khoan-dto.model';
-import { ApiResponse } from '../../model/ApiResponse';
-import { ErrorCode } from '../../model/ErrorCode';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
+import {TaiKhoanService} from '../../service/TaiKhoanService';
+import {TaiKhoanDto} from '../../model/tai-khoan-dto.model';
+import {ApiResponse} from '../../model/ApiResponse';
+import {ErrorCode} from '../../model/ErrorCode';
 
 @Component({
     selector: 'app-register',
@@ -49,25 +49,38 @@ export class RegisterComponent {
             return;
         }
 
-        this.accountService.createAccount(accountData)
-            .subscribe(
-                (response: ApiResponse<TaiKhoanDto>) => {
-                    this.successMessage = 'Đăng ký thành công! Chuyển hướng đến trang đăng nhập...';
-                    setTimeout(() => {
-                        this.router.navigate(['/log-in']);
-                    }, 2000);
-                },
-                (error: HttpErrorResponse) => {
-                    if (error.error.code === ErrorCode.ACCOUNT_EXISTED) {
-                        this.errorMessage = 'Tài khoản đã tồn tại.';
-                    } else if (error.error.code === ErrorCode.USERNAME_INVALID) {
-                        this.errorMessage = 'Tên đăng nhập không hợp lệ.';
-                    } else if (error.error.code === ErrorCode.PASSWORD_INVALID) {
-                        this.errorMessage = 'Mật khẩu không hợp lệ.';
-                    } else {
-                        this.errorMessage = 'Đã xảy ra lỗi, vui lòng thử lại sau.';
-                    }
+        const username = this.accountForm.value.tenDangNhap;
+
+        this.accountService.checkUsernameExists(username).subscribe(
+            (response: ApiResponse<any>) => {
+                if (response.result) {
+                    this.errorMessage = 'Tên đăng nhập đã tồn tại.';
+                } else {
+                    this.accountService.createAccount(accountData)
+                        .subscribe(
+                            (response: ApiResponse<TaiKhoanDto>) => {
+                                this.successMessage = 'Đăng ký thành công! Chuyển hướng đến trang đăng nhập...';
+                                setTimeout(() => {
+                                    this.router.navigate(['/log-in']);
+                                }, 2000);
+                            },
+                            (error: HttpErrorResponse) => {
+                                if (error.error.code === ErrorCode.ACCOUNT_EXISTED) {
+                                    this.errorMessage = 'Tài khoản đã tồn tại.';
+                                } else if (error.error.code === ErrorCode.USERNAME_INVALID) {
+                                    this.errorMessage = 'Tên đăng nhập không hợp lệ.';
+                                } else if (error.error.code === ErrorCode.PASSWORD_INVALID) {
+                                    this.errorMessage = 'Mật khẩu không hợp lệ.';
+                                } else {
+                                    this.errorMessage = 'Đã xảy ra lỗi, vui lòng thử lại sau.';
+                                }
+                            }
+                        );
                 }
-            );
+            },
+            (error: HttpErrorResponse) => {
+                this.errorMessage = 'Đã xảy ra lỗi khi kiểm tra tên đăng nhập, vui lòng thử lại sau.';
+            }
+        );
     }
 }
