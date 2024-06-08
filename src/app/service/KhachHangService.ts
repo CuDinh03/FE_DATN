c4982c2f298adb10172b3e8f5b4ce2c75cc68e57
Vuong Nguyen <vuongnv28@gmail.com>
2024-06-08 11:02:55 +0700

    quan ly - update thong tin khach hang

--- src/app/service/KhachHangService.ts
+++ src/app/service/KhachHangService.ts
@@ -1,13 +1,13 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
-import {ApiResponse} from "../model/ApiResponse";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
-import {KhachHangDto} from "../model/khachHangDto";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ApiResponse } from "../model/ApiResponse";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { KhachHangDto } from "../model/khachHangDto";
 
 @Injectable({
   providedIn: 'root'
 })
-export class KhachHangService{
+export class KhachHangService {
 
   apiUrl = 'http://localhost:9091/api/khs';
 
@@ -21,41 +21,64 @@ export class KhachHangService{
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
-    
+
     let params = new HttpParams();
     params = params.append('page', page.toString());
     params = params.append('size', size.toString());
 
-    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/all`, {params, headers});
+    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/all`, { params, headers });
   }
-  
-  getKhachHang(sdt: string): Observable<ApiResponse<any>>{
+
+  getKhachHang(sdt: string): Observable<ApiResponse<any>> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
-    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/${sdt}`, {headers});
+    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/${sdt}`, { headers });
   }
 
-  themKhachHang(khach: KhachHangDto):Observable<ApiResponse<KhachHangDto>>{
+  themKhachHang(khach: KhachHangDto): Observable<ApiResponse<KhachHangDto>> {
 
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
-    return this.http.post<ApiResponse<KhachHangDto>>(this.apiUrl + '/create', khach,{headers});
+    return this.http.post<ApiResponse<KhachHangDto>>(this.apiUrl + '/create', khach, { headers });
 
   }
-
 
   // ==> Call api Lấy ra khách hàng từ Id Tài khoản
-  getKhachHangByIdTaiKhoan(idTaiKhoan : string): Observable<ApiResponse<KhachHangDto>> {
-    
+  getKhachHangByIdTaiKhoan(idTaiKhoan: string): Observable<ApiResponse<KhachHangDto>> {
+
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
 
-    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/getKHByIdTaiKhoan/${idTaiKhoan}`, {headers});
+    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/getKHByIdTaiKhoan/${idTaiKhoan}`, { headers });
   }
+
+
+  // ==> Lấy khách hàng theo id 
+  getKhachHangByID(id: string): Observable<ApiResponse<any>> {
+
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get<ApiResponse<any>>(`${this.apiUrl}/detail/${id}`, { headers });
+  }
+
+  // ==> Update khách hàng
+  suaKhachHang(id: string, khachHang: any): Observable<ApiResponse<any>> {
+
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    
+    return this.http.put<ApiResponse<any>>(`${this.apiUrl}/update/${id}`,khachHang, { headers });
+  }
+
 }
