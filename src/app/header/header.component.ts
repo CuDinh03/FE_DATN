f5e335e660844c4759a22e012a202db7ee659e3e
Lê Anh Tuấn <114102305+anhtuando19@users.noreply.github.com>
2024-06-08 07:46:14 +0700

    update view giỏ hàng roles customer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,12 @@
+import { ErrorCode } from './../model/ErrorCode';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ApiResponse } from './../model/ApiResponse';
+import { KhachHangService } from './../service/KhachHangService';
+import { AuthenticationService } from './../service/AuthenticationService';
+import { GioHangService } from 'src/app/service/GioHangService';
+import { GioHangChiTietService } from './../service/GioHangChiTietService';
 import { Component } from '@angular/core';
-import {AuthenticationService} from "../service/AuthenticationService";
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -10,14 +16,86 @@ import {Router} from "@angular/router";
 export class HeaderComponent {
   showSearch: boolean = false;
   isLoggedInCart: boolean = false;
+  isCartHovered = false;
+  khachHang: any;
+  gioHang: any
+  gioHangChiTiet: any[] = [];
 
-  constructor(private auth: AuthenticationService, private router: Router) {
+
+  constructor(private auth: AuthenticationService, private router: Router,
+    private gioHangChiTietService: GioHangChiTietService,
+    private gioHangService: GioHangService,
+    private khachHangService: KhachHangService
+  ) {
+    //   const tenDangNhap = localStorage.getItem('tenDangNhap');
+    // if (tenDangNhap) {
+    //   this.khachHangService.findKhachHangByTenDangNhap(tenDangNhap).subscribe(
+    //     (response) => {
+    //       this.customer = response.result;
+    //       // Xử lý dữ liệu khách hàng ở đây
+    //       console.log(this.customer);
+    //     },
+    //     (error) => {
+    //       console.error('Error fetching customer:', error);
+    //     }
+    //   );
+    // }
+
   }
 
   ngOnInit() {
     // Kiểm tra trạng thái đăng nhập của người dùng
     this.checkLoginStatus();
+    this.findShoppingCart();
   }
+
+  findShoppingCart() {
+    const tenDangNhap = this.auth.getTenDangNhap();
+    if (tenDangNhap) {
+      this.khachHangService.findKhachHangByTenDangNhap(tenDangNhap).subscribe(
+        (response) => {
+          const khachHang = response.result;
+          if (khachHang && khachHang.id) {
+            this.gioHangService.findGioHangByIdKhachHang(khachHang.id).subscribe(
+              (response) => {
+                const gioHang = response.result;
+                console.log(gioHang);
+                if (gioHang && gioHang.id){
+                  this.loadGioHangChiTiet(gioHang.id);
+                }
+              },
+              (error) => {
+                console.error('Error fetching shopping cart:', error);
+              }
+            );
+          } else {
+            console.error('Không tìm thấy thông tin khách hàng.');
+          }
+        },
+        (error) => {
+          console.error('Error fetching customer:', error);
+        }
+      );
+    }
+  }
+
+  loadGioHangChiTiet(idGioHang: string): void {
+    this.gioHangChiTietService.getAll(idGioHang).subscribe(
+      (response: ApiResponse<any>) => {
+        if (response.result && response.result.length > 0) {
+          this.gioHangChiTiet = response.result;
+          console.log(this.gioHangChiTiet);
+        } else {
+          this.gioHangChiTiet = [];
+        }
+      },
+      (error: HttpErrorResponse) => {
+          console.error('Unexpected error:', error);
+      }
+    );
+  }
+
+
 
   toggleSearch() {
     this.showSearch = !this.showSearch;
@@ -28,15 +106,29 @@ export class HeaderComponent {
   }
 
   isLoggedIn(): boolean {
-   return this.auth.isLoggedIn();
+    return this.auth.isLoggedIn();
   }
 
-  getAccount(): boolean{
-    if (this.auth.getRole() == 'ADMIN'){
+  getAccount(): boolean {
+    if (this.auth.getRole() == 'ADMIN') {
       return true;
     }
     return false;
 
+  }
+
+  onMouseOver() {
+    this.isCartHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isCartHovered = false;
+  }
+
+  getCartTotal(): number {
+    return this.gioHangChiTiet.reduce((total, item) => {
+      return total + item.soLuong * item.chiTietSanPham.giaBan;
+    }, 0);
   }
 
   logout() {
@@ -54,6 +146,10 @@ export class HeaderComponent {
     }).catch(err => {
       console.error('Error navigating to /trang-chu:', err);
     });
+  }
+
+  getTotalQuantity(): number {
+    return this.gioHangChiTiet.reduce((total, item) => total + item.soLuong, 0);
   }
 
 }
--- src/app/page/shopping-cart/shopping-cart.component.ts
+++ src/app/page/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,12 @@
+import { ApiResponse } from './../../model/ApiResponse';
+import { HttpErrorResponse } from '@angular/common/http';
+import { KhachHangService } from './../../service/KhachHangService';
+import { GioHangService } from 'src/app/service/GioHangService';
+import { GioHangChiTietService } from './../../service/GioHangChiTietService';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../../service/AuthenticationService';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,5 +14,117 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent {
+  showSearch: boolean = false;
+  isLoggedInCart: boolean = false;
+  isCartHovered = false;
+  khachHang: any;
+  gioHang: any
+  gioHangChiTiet: any[] = [];
 
+
+  constructor(private auth: AuthenticationService, private router: Router,
+    private gioHangChiTietService: GioHangChiTietService,
+    private gioHangService: GioHangService,
+    private khachHangService: KhachHangService
+  ) {
+    
+
+  }
+
+  ngOnInit() {
+    // Kiểm tra trạng thái đăng nhập của người dùng
+    this.checkLoginStatus();
+    this.findShoppingCart();
+  }
+
+  calculateSubtotal(item: any): number {
+    return item.chiTietSanPham.giaBan * item.soLuong;
+  }
+  
+  findShoppingCart() {
+    const tenDangNhap = this.auth.getTenDangNhap();
+    if (tenDangNhap) {
+      this.khachHangService.findKhachHangByTenDangNhap(tenDangNhap).subscribe(
+        (response) => {
+          const khachHang = response.result;
+          if (khachHang && khachHang.id) {
+            this.gioHangService.findGioHangByIdKhachHang(khachHang.id).subscribe(
+              (response) => {
+                const gioHang = response.result;
+                console.log(gioHang);
+                if (gioHang && gioHang.id){
+                  this.loadGioHangChiTiet(gioHang.id);
+                }
+              },
+              (error) => {
+                console.error('Error fetching shopping cart:', error);
+              }
+            );
+          } else {
+            console.error('Không tìm thấy thông tin khách hàng.');
+          }
+        },
+        (error) => {
+          console.error('Error fetching customer:', error);
+        }
+      );
+    }
+  }
+
+  loadGioHangChiTiet(idGioHang: string): void {
+    this.gioHangChiTietService.getAll(idGioHang).subscribe(
+      (response: ApiResponse<any>) => {
+        if (response.result && response.result.length > 0) {
+          this.gioHangChiTiet = response.result;
+          console.log(this.gioHangChiTiet);
+        } else {
+          this.gioHangChiTiet = [];
+        }
+      },
+      (error: HttpErrorResponse) => {
+          console.error('Unexpected error:', error);
+      }
+    );
+  }
+
+
+
+  toggleSearch() {
+    this.showSearch = !this.showSearch;
+  }
+  checkLoginStatus() {
+    const token = localStorage.getItem('token');
+    this.isLoggedInCart = !!token;
+  }
+
+  isLoggedIn(): boolean {
+    return this.auth.isLoggedIn();
+  }
+
+  getAccount(): boolean {
+    if (this.auth.getRole() == 'ADMIN') {
+      return true;
+    }
+    return false;
+
+  }
+
+  onMouseOver() {
+    this.isCartHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isCartHovered = false;
+  }
+
+  getCartTotal(): number {
+    return this.gioHangChiTiet.reduce((total, item) => {
+      return total + item.soLuong * item.chiTietSanPham.giaBan;
+    }, 0);
+  }
+
+
+  getTotalQuantity(): number {
+    return this.gioHangChiTiet.reduce((total, item) => total + item.soLuong, 0);
+  }
 }
