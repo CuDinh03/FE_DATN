8c75106c92e0ddac28845b440404b167f77294ad
CuDinh03 <dinhhuycu0305@gmail.com>
2024-06-15 02:31:06 +0700

    update payment

---
+++ src/app/page/payment-view/payment-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-view',
+  templateUrl: './payment-view.component.html',
+  styleUrls: ['./payment-view.component.css']
+})
+export class PaymentViewComponent {
+
+}
